43c826e380e5bfa71752f5303b434bcd597870c5
mayanksinha11 <38779878+mayanksinha11@users.noreply.github.com>
2018-05-02 17:00:13 +0530

    Update irmqttdatareceivescript.py

--- irmqttdatareceivescript.py
+++ irmqttdatareceivescript.py
@@ -1,3 +1,4 @@
+# This script receives and processes the data that the NODEMCU module (that has the IR BEAM break sensor attached to it) sends via MQTT protocol
 import paho.mqtt.client as mqtt
 from twilio.rest import Client
 import time
@@ -13,14 +14,16 @@ GPIO.setup(21,GPIO.IN)
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
  
-    
+    #Mqtt topic
     client.subscribe("esp/test")
     client.subscribe("esp/test")
  
 
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
+  
     if GPIO.input(21) and GPIO.input(24):
+    # this condition is used to halt the sysytem when both the face recognition and the fingerprint module give a positive answer for recognition of the user
         print("Hello user, the system is halted for 30 seconds")
         time.sleep(30)
 
@@ -28,15 +31,15 @@ def on_message(client, userdata, msg):
         print("Motion detected, intruder alert")
         
         from twilio.rest import Client
-        
+        # Twilio web service packages and libraries to make the call to the user
         account_sid = "AC0ae86014fe91789e86a3e4195125709f"
         auth_token = "0b3c35d9918e9d4fa12d1d454f7e60ba"
         client = Client(account_sid, auth_token)
         print "calling....."
         
         call = client.calls.create(
-        to="+919837311207",
-        from_="+18722013290",
+        to="The number to be called",
+        from_="Your Twilio number",
         url="http://demo.twilio.com/docs/voice.xml"
         )
         i=1
@@ -68,6 +71,6 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
  
-client.connect("192.168.1.102", 1883, 60)
+client.connect("IP of the Raspberry pi on which the MQTT server is running", 1883, 60)
  
 client.loop_forever()
